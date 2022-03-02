7bc4d407333dd4b3623043f834b8e878dc7d8233
emhaarifin <101muhammadarifin@gmail.com>
2022-03-02 20:55:34 +0700

    feat: add component for infinite scroll

---
+++ src/components/LoadOnScoll/index.tsx
@@ -0,0 +1,42 @@
+import React, { useCallback, useRef } from 'react';
+
+const LoadOnScroll: React.FC<{
+  onLoad?: () => void;
+  loading?: boolean;
+  hashMore?: boolean;
+}> = ({ onLoad, loading, hashMore }) => {
+  const observer = useRef<IntersectionObserver | null>(null);
+
+  const componentRef = useCallback(
+    (node) => {
+      if (loading) return;
+
+      if (observer && observer.current) {
+        observer.current.disconnect();
+      }
+
+      observer.current = new IntersectionObserver(
+        (entries) => {
+          if (entries[0].isIntersecting) {
+            onLoad && onLoad();
+          }
+        },
+        {
+          root: null,
+          rootMargin: '20px',
+          threshold: 1.0,
+        }
+      );
+
+      if (node) {
+        observer.current.observe(node);
+        if (!hashMore) observer?.current?.unobserve(node);
+      }
+    },
+    [hashMore, loading, onLoad]
+  );
+
+  return <div ref={componentRef} className='h-20'></div>;
+};
+
+export default LoadOnScroll;
