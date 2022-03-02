5224cfff8d35e0c7b263f5948419244f9095fd26
emhaarifin <101muhammadarifin@gmail.com>
2022-03-02 20:56:08 +0700

    style: improve to handle height initial load

--- src/components/ImageBook/index.tsx
+++ src/components/ImageBook/index.tsx
@@ -8,7 +8,14 @@ interface ImageBookProps {
 const ImageBook: React.FC<ImageBookProps> = ({ alt, className, src }) => {
   return (
     <figure className={className}>
-      <img alt={alt} className='rounded-lg h-full bg-slate-200' style={{ aspectRatio: '4/6' }} src={src}></img>
+      <img
+        alt={alt}
+        className='rounded-lg bg-slate-200'
+        width={400}
+        height={600}
+        style={{ aspectRatio: '4/6' }}
+        src={src}
+      ></img>
     </figure>
   );
 };
