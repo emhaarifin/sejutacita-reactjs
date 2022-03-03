690874cbfbc05bc94665f4cf1f790b2aec4d17f0
emhaarifin <101muhammadarifin@gmail.com>
2022-03-03 23:38:15 +0700

    feat: integration hooks

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,9 +1,13 @@
+import useSearch from 'hooks/useSearch';
 import React from 'react';
 
 const Search: React.FC = () => {
+  const { handleSubmit } = useSearch();
   return (
-    <form className='flex pr-2 flex-1 h-10'>
+    <form className='flex pr-2 flex-1 h-10' onSubmit={handleSubmit}>
       <input
+        type='text'
+        name='search'
         className='bg-slate-50 px-4 text-lg border border-slate-200 flex-1 h-full rounded-full '
         placeholder='Search'
       ></input>
