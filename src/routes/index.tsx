56eb5562e9b1b74bf6b7318c2dc21d952452a261
emhaarifin <101muhammadarifin@gmail.com>
2022-03-03 23:27:53 +0700

    feat: routing web app

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,31 @@
-import React, { lazy } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-const Start = lazy(() => import('pages/Start'));
+import LayoutExplore from 'layouts/explore';
+import React, { lazy, Suspense } from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+const PageHome = lazy(() => import('pages/Home'));
+const PageDetailBook = lazy(() => import('pages/DetailBook'));
+const Layout = lazy(() => import('layouts/default'));
+const ErrorBoundary = lazy(() => import('pages/ErrorBoundary'));
+const PageBookmark = lazy(() => import('pages/Bookmark'));
+const PageExplore = lazy(() => import('pages/Explore'));
+
 const BookuRoutes: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/start' element={<Start />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <Suspense fallback={<></>}>
+      <ErrorBoundary>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Layout />}>
+              <Route element={<LayoutExplore />}>
+                <Route index element={<PageHome />} />
+                <Route path='explore/:category_id' element={<PageExplore />} />
+              </Route>
+              <Route path='bookmark' element={<PageBookmark />} />
+              <Route path='book/:category_id/:id' element={<PageDetailBook />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </ErrorBoundary>
+    </Suspense>
   );
 };
 
