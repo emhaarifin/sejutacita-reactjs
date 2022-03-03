7c1b752b318cdb22bed4a6af42132e1a98d2fdce
emhaarifin <101muhammadarifin@gmail.com>
2022-03-04 01:04:32 +0700

    refactor: use books components

--- src/pages/Bookmark/index.tsx
+++ src/pages/Bookmark/index.tsx
@@ -1,4 +1,4 @@
-import CardBook from 'components/CardBook';
+import Books from 'components/Books';
 import React, { Fragment, useEffect, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from 'store';
@@ -7,30 +7,30 @@ import getCategoryById from 'utils/getCategoryById';
 
 const PageBookmark: React.FC = () => {
   const userState = useSelector((state: RootState) => state.user);
-  const categoryState = useSelector((state: RootState) => state.categories);
-
+  const categoriesState = useSelector((state: RootState) => state.categories);
   const refBooks = useRef(userState.readingBooks);
-  const refCategory = useRef(categoryState.categories);
-  const [books, setBooks] = useState<readingBooks[]>(userState.readingBooks);
+  const refCategories = useRef(categoriesState.categories);
+  const [books, setBooks] = useState<readingBooks[]>([]);
   const [filterBy, setFilterBy] = useState<string>('all');
 
-  const handleFilter = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setFilterBy(e.target.value);
+  const handleFilter = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setFilterBy(event.target.value);
   };
+
   useEffect(() => {
     filterBy === 'all'
       ? setBooks(refBooks.current)
       : setBooks(
-          refBooks.current.filter((book) => getCategoryById(refCategory.current, book.category_id)?.name === filterBy)
+          refBooks.current.filter((book) => getCategoryById(refCategories.current, book.category_id)?.name === filterBy)
         );
-  }, [filterBy]);
+  }, [categoriesState.categories, filterBy]);
 
   return (
     <Fragment>
       <section className='flex justify-end'>
         <select onChange={handleFilter} className='px-3 py-2 border bg-slate-50 border-slate-300 rounded-full '>
           <option value='all'>All</option>
-          {categoryState.categories.map((category) => (
+          {categoriesState.categories.map((category) => (
             <option key={category.id} value={category.name}>
               {category.name}
             </option>
@@ -38,32 +38,15 @@ const PageBookmark: React.FC = () => {
         </select>
       </section>
       <div className='my-5'></div>
-      <section>
-        {books.length > 0 && books.find((book) => book.isBookmark) ? (
-          <Fragment>
-            <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4'>
-              {books.map(
-                (book) =>
-                  book.isBookmark && (
-                    <CardBook
-                      key={`${book.id} - ${book.category_id}`}
-                      to={`/book/${book.category_id}/${book.id}`}
-                      state={{ book: book }}
-                      image={book.cover_url}
-                      category={getCategoryById(categoryState.categories, book.category_id)?.name as string}
-                      title={book.title}
-                      author={book.authors[0]}
-                    />
-                  )
-              )}
-            </div>
-          </Fragment>
-        ) : (
+      <Books
+        books={books.filter((book) => book.isBookmark)}
+        loading={false}
+        RenderEmpty={() => (
           <div className='flex flex-col items-center justify-center'>
-            <span className='flex-1'>Don't have a bookmark list</span>
+            <span className='flex-1'>Don't have a bookmark list with {filterBy} category</span>
           </div>
         )}
-      </section>
+      />
     </Fragment>
   );
 };
